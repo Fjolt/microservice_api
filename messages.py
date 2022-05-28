21a918dd5693441cbc2019cfded7ab4699ad6de7
Fjolt <54676330+Fjolt@users.noreply.github.com>
2022-05-28 21:07:22 +0200

    Delete messages.py

--- messages.py
+++
@@ -1,9 +0,0 @@
-# error messages pre api
-
-
-messages = {
-    "404 post": "We could not find this post.Try typing in its ID or the ID of its creator.",
-    "409": "Post id already taken",
-    "404 user": "User does not exist",
-    "401": "You are not authorised to delete this post."
-}
