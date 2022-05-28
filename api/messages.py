f2a628a5bbe6f420e81a9b6ea7039a63a6937403
Fjolt <54676330+Fjolt@users.noreply.github.com>
2022-05-28 21:11:02 +0200

    Add files via upload

---
+++ api/messages.py
@@ -0,0 +1,10 @@
+# error messages pre api
+
+
+messages = {
+    "404 post": "We could not find this post.Try typing in its ID or the ID of its creator.",
+    "409": "Post id already taken",
+    "404 user": "User does not exist",
+    "401": "You are not authorised to delete this post.",
+    "401 update": "You are not authorised to update this post."
+}
